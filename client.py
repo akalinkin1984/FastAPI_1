297a9aff893d25052d4b49325e0e365db6c4098b
Alexandr Kalinkin <akalinkin84@yandex.ru>
2024-09-30 18:03:07 +0300

    3

--- client.py
+++ client.py
@@ -4,25 +4,25 @@ import requests
 response = requests.post(
     "http://127.0.0.1:8080/advertisement",
     json={
-        'title': 'New',
-        'description': 'TV',
-        'price': 125.50,
-        'author': 'Yura'
+        'title': 'Telephone',
+        'description': 'Samsung',
+        'price': 128.75,
+        'author': 'Ivan'
     }
 )
 
-# response = requests.get("http://127.0.0.1:8080/advertisement/1")
+# response = requests.get("http://127.0.0.1:8080/advertisement/3")
 
-# response = requests.patch("http://127.0.0.1:8080/advertisement/1",
+# response = requests.patch("http://127.0.0.1:8080/advertisement/3",
 #                           json={
 #                               'description': 'Iphone New Model',
-#                               'price': 130.0,
+#                               'price': 135.0,
 #                                 }
 #                           )
 
 # response = requests.delete("http://127.0.0.1:8080/advertisement/1")
 
-# response = requests.get("http://127.0.0.1:8080/advertisement?qs_params=TV")
+# response = requests.get("http://127.0.0.1:8080/advertisement?qs_params=Sam")
 
 print(response.status_code)
 print(response.json())
